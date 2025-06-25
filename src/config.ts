3cf5b078bd4c843f02de2cd944eb64dc1406e4d3
Riza Pranata <corodotc@gmail.com>
2025-06-25 14:56:03 +0700

    modify config env

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,5 @@
 const configEnv = {
   api_host: process.env.REACT_APP_API_HOST,
-  access_token: process.env.REACT_APP_GITHUB_TOKEN,
-  owner: process.env.REACT_APP_SITE_AUTHOR,
 };
 
 export { configEnv };
